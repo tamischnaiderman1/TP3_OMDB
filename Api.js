07b3280b0e06e1ac6baeb60654fd1fba3b49abf1
TAMI VIOLETA SCHNAIDERMAN <47026792@ort>
2023-05-08 15:58:41 -0300

    first commit

--- Api.js
+++ Api.js
@@ -1,19 +1,23 @@
-function Buscar(event) {
+function buscar(event) {
     let palabraBuscada = document.getElementById("busqueda").value
-    let arrayResultados = []
 
     let tipo = document.getElementById("tipoDeContenido").value
 
+    if (tipo == "default") {
 
-    if (tipo == null) {
+        document.getElementById("peliculas").innerHTML = ""
 
-        fetch("https://www.omdbapi.com/?apikey=165ae8f&" + "t=" + palabraBuscada)
+        fetch("https://www.omdbapi.com/?apikey=165ae8f&" + "s=" + palabraBuscada)
             .then(res => res.json())
             .then(res => {
-                arrayResultados.push(res)
                 console.log("obtuve respuesta")
-                valores = document.getElementById("valores");
-                arrayResultados.forEach(element => {
+                res.Search.forEach(elemento => {
+                    let card = document.createElement("div").innerHTML=`<div class="card">
+                    <img src="${elemento.Poster}" alt="Card Image">
+                    <h2>${elemento.Title}</h2>
+                    <p>${elemento.Year}</p>
+                    </div>`
+                    document.getElementById("peliculas").innerHTML+=card
                 })
             })
             .catch(err => console.error("error", err))
@@ -23,23 +27,30 @@ function Buscar(event) {
     }
 
     else {
+        
 
-        fetch("https://www.omdbapi.com/?apikey=165ae8f&" + "t=" + palabraBuscada + "type=" + tipo)
+        fetch("https://www.omdbapi.com/?apikey=165ae8f&" + "t=" + palabraBuscada +"&"+ "type=" + tipo)
             .then(res => res.json())
             .then(res => {
                 console.log("obtuve respuesta")
-                valores = document.getElementById("valores");
-                res.forEach(actual => {
-                    if (actual.compra && actual.venta) {
-                        dolar = document.createElement("li")
-                        dolar.innerHTML = `Nombre: ${actual.nombre} - compra: ${actual.compra} - venta: ${actual.venta}`
-                        valores.appendChild(dolar)
-                    }
-                })
+                document.getElementById("peliculas").innerHTML = ""
+
+    
+                let card = document.createElement("div").innerHTML=`<div class="card" style="width: 18rem;">
+                <img src="${res.Poster}" class="card-img-top" >
+                <div class="card-body">
+                    <h5 class="card-title">${res.Title}</h5>
+                    <p class="card-text">Tipo: ${res.Type} <br>
+                    Lanzamiento: ${res.Year}
+                    </p>
+                </div>
+                </div>`
+                console.log(card)
+                    document.getElementById("peliculas").innerHTML+=card
             })
             .catch(err => console.error("error", err))
         console.log("Fin consulta - fetch")
-
+            
     }
 
 }
